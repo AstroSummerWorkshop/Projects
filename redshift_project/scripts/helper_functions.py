39ae5412fec7fdb02d869285d2079981f2488314
natalielamwy <natalielam.wy@gmail.com>
2025-07-14 18:00:47 -0700

    Bulk of spectroscopy redshift project (Jupyter notebook, scripts, images, catalog, data zip file)

--- redshift_project/scripts/helper_functions.py
+++ redshift_project/scripts/helper_functions.py
@@ -1,4 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 from astropy.table import Table
+from astropy.io import fits
+import pandas as pd
+import os
+import sys
+from glob import glob
+    
 
 pretty_plot_params = {'font.size': 18, 
                      'text.usetex': True, 
@@ -29,9 +38,86 @@ def read_spectrum(filename):
     astropy.table.Table: The spectrum data.
     """
     
-    t = Table.read(filename, hdu='COADD')
+    t = Table.read(filename)
 
-    wavelength = 10**t['loglam']
+    wavelength = t['wavelength']
     flux = t['flux']
 
     return wavelength, flux
+
+# Pre-made function to plot a spectrum
+def plot_spectrum(filename, wave_range=None, fig=None, ax=None):
+    """
+    Plots the spectrum from the given filename.
+
+    Parameters:
+        filename (str): Path to the spectrum file.
+        wave_range (tuple, optional): (wave_min, wave_max) for x-axis limits.
+        fig (matplotlib.figure.Figure, optional): Existing figure to use.
+        ax (matplotlib.axes.Axes, optional): Existing axes to use.
+
+    Returns:
+        fig, ax: The figure and axis containing the plot.
+    """
+    wave, flux = read_spectrum(filename)
+
+    # Apply wavelength range if specified
+    if wave_range is not None:
+        wave_min, wave_max = wave_range
+
+        # Create a mask to isolate the wavelength region
+        mask = (wave >= wave_min) & (wave <= wave_max)
+
+        # Apply the mask to the wavelength and flux arrays
+        wave = wave[mask]
+        flux = flux[mask]
+
+    # Create figure and axis if not provided
+    if ax is None or fig is None:
+        fig, ax = plt.subplots(figsize=(6, 4))
+
+    # Plot the spectrum
+    ax.step(wave, flux, where='mid', color='black', lw=1)
+    ax.set_xlabel('Wavelength [Å]')
+    ax.set_ylabel('Flux')
+
+    if wave_range is not None:
+        ax.set_xlim(wave_min, wave_max)
+
+    return fig, ax
+
+
+# Define a Gaussian function for fitting
+# A Gaussian is also the bell curve
+def gaussian(x, amp, cen, wid, offset):
+  """
+  x : an array of wavelengths
+  amp : amplitude of the Gaussian
+  cen : center of the Gaussian (mean)
+  wid : width of the Gaussian (standard deviation)
+  offset : vertical offset of the Gaussian
+
+  Returns the Gaussian array.
+  """
+  return amp * np.exp(-(x-cen)**2 / (2*wid**2)) + offset
+
+
+# Pre-made function to fit Gaussian profile to an emission line
+def fit_gaussian(filename, wave_range=[4980, 5020]):
+  wave, flux = read_spectrum(filename)
+
+  wave_min, wave_max = wave_range
+  # Create a mask to isolate the wavelength region
+  mask = (wave >= wave_min) & (wave <= wave_max)
+  # Apply the mask to the wavelength and flux arrays
+  wave, flux = wave[mask], flux[mask]
+
+  # Fit a Gaussian profile to the emission line
+  p0 = [1, 500, 10, 0]  # Initial guess for the parameters
+  fit, cov = curve_fit(gaussian, wave, flux, p0=p0)
+  # fit contains the best-fit parameters (amp, cen, wid, offset)
+  # cov is the "covariance" of the fit
+  # We can turn cov into the "error" of the fit like this:
+  fit_err = np.sqrt(np.diag(cov))
+
+  return fit, fit_err
